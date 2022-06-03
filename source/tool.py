c6dba1d175b4abe6e9e7f4948204b35ad16930e2
星外之神 <wszqkzqk@qq.com>
2022-06-03 19:54:38 +0800

    使用flip更新

--- source/tool.py
+++ source/tool.py
@@ -97,7 +97,7 @@ class Control():
         while not self.done:
             self.event_loop()
             self.update()
-            pg.display.update()
+            pg.display.flip()
             self.clock.tick(self.fps)
         print('game over')
 
